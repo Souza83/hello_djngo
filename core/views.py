a79586c169485e13db835bfb97bd433fdc3d6368
Souza <tec.psouza@gmail.com>
2021-11-06 08:24:44 -0300

    Inserido novos metodos de calculos aritmeticos por parametros em url

--- core/views.py
+++ core/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
+
 def hello(request, nome, idade):
     return HttpResponse('<center><h1>Hello {} de {} anos</h1></center>'.format(nome, idade))
+
+
+def soma(request, num1, num2):
+    resultado = num1 + num2
+    return HttpResponse('<center><h1>A soma de {} + {} = {}</h1></center>'.format(num1, num2, resultado))
+
+
+def subtracao(request, num1, num2):
+    resultado = num1 - num2
+    return HttpResponse('<center><h1>A subtração de {} - {} = {}</h1></center>'.format(num1, num2, resultado))
+
+
+def multiplicacao(request, num1, num2):
+    resultado = num1 * num2
+    return HttpResponse('<center><h1>A multiplicação de {} x {} = {}</h1></center>'.format(num1, num2, resultado))
+
+
+def divisao(request, num1, num2):
+    resultado = num1 / num2
+    return HttpResponse('<center><h1>A divisão de {} / {} = {}</h1></center>'.format(num1, num2, resultado))
